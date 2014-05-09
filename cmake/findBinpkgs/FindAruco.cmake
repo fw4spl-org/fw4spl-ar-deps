74dcee512e2c44bedba0157fdf621561b2ac8217
fchamp@IRCAD.FR <fchamp@IRCAD.FR>
2014-05-09 16:33:39 +0200

    Fixed the find of aruco library
    --HG--
    branch : arflex_0.1

--- cmake/findBinpkgs/FindAruco.cmake
+++ cmake/findBinpkgs/FindAruco.cmake
@@ -2,7 +2,7 @@ INCLUDE_DIRECTORIES()
 SET(aruco_INCLUDE_DIRS )
 
 SET(aruco_LIBS opencv_contrib;opencv_legacy;opencv_stitching;opencv_gpu;opencv_objdetect;opencv_calib3d;opencv_features2d;opencv_video;opencv_highgui;opencv_ml;opencv_imgproc;opencv_flann;opencv_core) 
-find_library(aruco_LIB aruco124 ${CMAKE_CURRENT_LIST_DIR}/lib/ )
+find_library(aruco_LIB aruco aruco124 ${CMAKE_CURRENT_LIST_DIR}/lib/ )
 SET(aruco_LIBS ${aruco_LIBS};${aruco_LIB})
 
 SET(aruco_VERSION        1.2.4)
