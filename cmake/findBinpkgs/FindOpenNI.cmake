df67bdb640fccf974493142b9270c4970d6fe2db
Thibaut Schmitt <thibaut.schmitt@ihu-strasbourg.eu>
2015-04-27 17:05:34 +0200

    Fixed tabs
    --HG--
    branch : fw4spl_0.10.2

--- cmake/findBinpkgs/FindOpenNI.cmake
+++ cmake/findBinpkgs/FindOpenNI.cmake
@@ -11,14 +11,14 @@ find_package(PkgConfig QUIET)
 
 # Find lib
 find_library(OPENNI_LIBRARY 
-	  NAMES OpenNI2
-          PATH_SUFFIXES lib/openni2/
+    NAMES OpenNI2
+    PATH_SUFFIXES lib/openni2/
 )
 
 # Find include
 find_path(OPENNI_INCLUDE_DIR 
-	  NAMES OpenNI.h
-          PATH_SUFFIXES include/openni2/
+    NAMES OpenNI.h
+    PATH_SUFFIXES include/openni2/
 )
 
 include(FindPackageHandleStandardArgs)
@@ -30,8 +30,8 @@ mark_as_advanced(OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
 
 
 if(OPENNI_FOUND)
-  # Add the include directories
-  set(OPENNI_INCLUDE_DIRS ${OPENNI_INCLUDE_DIR})
-  message(STATUS "OpenNI found (include: ${OPENNI_INCLUDE_DIRS}, lib: ${OPENNI_LIBRARIES})")
-endif(OPENNI_FOUND)
+# Add the include directories
+    set(OPENNI_INCLUDE_DIRS ${OPENNI_INCLUDE_DIR})
+    message(STATUS "OpenNI found (include: ${OPENNI_INCLUDE_DIRS}, lib: ${OPENNI_LIBRARIES})")
+endif()
 
