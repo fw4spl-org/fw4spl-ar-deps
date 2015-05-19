9adf9d9a77041228cb68d47dcd05224d8d43d4a1
Emilie Harquel <emilie.har@gmail.com>
2015-05-19 16:17:54 +0200

    Renamed FindOpenNI.cmake to FindOpenNI2.cmake.
    --HG--
    branch : fw4spl_0.10.2

--- cmake/findBinpkgs/FindOpenNI2.cmake
+++ cmake/findBinpkgs/FindOpenNI2.cmake
@@ -6,32 +6,30 @@
 # OPENNI_INCLUDE_DIRS - Directories containing the OPENNI include files.
 # OPENNI_LIBRARIES - Libraries needed to use OPENNI.
 
-
 find_package(PkgConfig QUIET)
 
 # Find lib
-find_library(OPENNI_LIBRARY 
+find_library(OPENNI2_LIBRARY
     NAMES OpenNI2
     PATH_SUFFIXES lib/openni2/
 )
 
 # Find include
-find_path(OPENNI_INCLUDE_DIR 
+find_path(OPENNI2_INCLUDE_DIR
     NAMES OpenNI.h
     PATH_SUFFIXES include/openni2/
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OpenNI DEFAULT_MSG OPENNI_LIBRARY OPENNI_INCLUDE_DIR
+find_package_handle_standard_args(OpenNI DEFAULT_MSG OPENNI2_LIBRARY OPENNI2_INCLUDE_DIR
 )
 
-mark_as_advanced(OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
+mark_as_advanced(OPENNI2_LIBRARY OPENNI2_INCLUDE_DIR)
 
 
 
 if(OPENNI_FOUND)
 # Add the include directories
-    set(OPENNI_INCLUDE_DIRS ${OPENNI_INCLUDE_DIR})
-    message(STATUS "OpenNI found (include: ${OPENNI_INCLUDE_DIRS}, lib: ${OPENNI_LIBRARIES})")
+    set(OPENNI2_INCLUDE_DIRS ${OPENNI2_INCLUDE_DIR})
+    message(STATUS "OpenNI found (include: ${OPENNI2_INCLUDE_DIRS}, lib: ${OPENNI2_LIBRARIES})")
 endif()
-
